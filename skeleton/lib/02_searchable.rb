de5828c772a50d9b629e080c08578658af7ac867
Sam Greenlee <sam.a.greenlee@gmail.com>
2016-04-29 14:46:22 -0400

    Implements Searchable module

--- skeleton/lib/02_searchable.rb
+++ skeleton/lib/02_searchable.rb
@@ -3,10 +3,25 @@ require_relative '01_sql_object'
 
 module Searchable
   def where(params)
-    # ...
+    param_string = params.keys.map { |name| "#{name} = :#{name}" }.join(" AND ")
+
+    query_string = <<-SQL
+      SELECT
+        *
+      FROM
+        #{table_name}
+      WHERE
+        #{param_string}
+    SQL
+
+    # puts query_string
+    # puts params
+    results = DBConnection.execute(query_string, params)
+    parse_all(results)
   end
+
 end
 
 class SQLObject
-  # Mixin Searchable here...
+  extend Searchable
 end
